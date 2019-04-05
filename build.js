6f326da592f2177efab4034a2e1e97746fe236da
Shrey Pasricha <shrey.pasricha@gmail.com>
2019-04-04 23:33:56 -0400

    Renamed and refactored into functions

---
+++ build.js
@@ -0,0 +1,27 @@
+var fs = require("fs");
+var minify = require('html-minifier').minify;
+
+// Minify index.html
+minifyHTML("src/index.html");
+
+function minifyHTML(src) {
+    fs.readFile(src, "utf8", function(err, data) {
+        if (err) throw err;
+
+        var htmlResult = minify(data, {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            removeComments: true,
+            removeOptionalTags: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeTagWhitespace: true,
+            useShortDoctype: true
+        });
+
+        fs.writeFile("dist/index.html", htmlResult, "utf8", (err) => {
+            if (err) throw err;
+            console.log("index.html has been minified.");
+        });
+    });
+}
