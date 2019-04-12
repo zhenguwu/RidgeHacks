f551ffeab9880733dc2192ce017f265b3c34880f
Shrey Pasricha <shrey.pasricha@gmail.com>
2019-04-11 23:25:27 -0400

    Refactored build.js to use find npm library.

--- build.js
+++ build.js
@@ -7,6 +7,8 @@ const postcss = require('postcss')
 
 const UglifyJS = require("uglify-js");
 
+const find = require('find');
+
 // Turn off variable name mangling, the default of true messes up some of the js libraries.
 const UglifyOptions = {
     mangle: {
@@ -14,23 +16,38 @@ const UglifyOptions = {
     }
 };
 
+// Enable HTML minification options.
+const HTML_minifyOptions = {
+    removeAttributeQuotes: true,
+    collapseWhitespace: true,
+    removeComments: true,
+    removeOptionalTags: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeTagWhitespace: true,
+    useShortDoctype: true
+}
+
 // Minify index.html
 minifyHTML("src/index.html");
 
 // Find and minify all CSS files
-fromDir('./src', /\.css$/, minifyCSS);
+find.eachfile(/\.css$/, './src', minifyCSS);
 
 // Find and minify all JS files.
-fromDir('./src', /\.js$/, minifyJS);
+find.eachfile(/\.js$/, './src', minifyJS);
 
-function minifyJS(src) {
-    const outputPath = "dist" + src.substring(3);
-
-    fs.readFile(src, "utf8", (err, js) => {
+function minifyHTML(src) {
+    fs.readFile(src, "utf8", function (err, data) {
         if (err) throw err;
-        console.log("\t\tWriting " + outputPath);
-        fs.writeFile(outputPath, UglifyJS.minify(js, UglifyOptions).code, (err) => { if (err) throw err; })
-    })
+
+        var htmlResult = minify(data, HTML_minifyOptions);
+
+        fs.writeFile("dist/index.html", htmlResult, "utf8", (err) => {
+            if (err) throw err;
+            console.log("index.html has been minified.\n");
+        });
+    });
 }
 
 function minifyCSS(src) {
@@ -42,7 +59,7 @@ function minifyCSS(src) {
         postcss([autoprefixer])
             .process(css, { from: src, to: outputPath })
             .then(result => {
-                console.log("\t\tWriting " + outputPath);
+                console.log("\t\tWriting minified CSS " + outputPath);
                 fs.writeFile(outputPath, result.css, (err) => { if (err) throw err; })
                 if (result.map) {
                     fs.writeFile(outputPath, result.map, (err) => { if (err) throw err; })
@@ -51,46 +68,12 @@ function minifyCSS(src) {
     });
 }
 
-function minifyHTML(src) {
-    fs.readFile(src, "utf8", function (err, data) {
+function minifyJS(src) {
+    const outputPath = "dist" + src.substring(3);
+
+    fs.readFile(src, "utf8", (err, js) => {
         if (err) throw err;
-
-        var htmlResult = minify(data, {
-            removeAttributeQuotes: true,
-            collapseWhitespace: true,
-            removeComments: true,
-            removeOptionalTags: true,
-            removeRedundantAttributes: true,
-            removeScriptTypeAttributes: true,
-            removeTagWhitespace: true,
-            useShortDoctype: true
-        });
-
-        fs.writeFile("dist/index.html", htmlResult, "utf8", (err) => {
-            if (err) throw err;
-            console.log("index.html has been minified.\n");
-        });
-    });
+        console.log("\t\tWriting minified JS " + outputPath);
+        fs.writeFile(outputPath, UglifyJS.minify(js, UglifyOptions).code, (err) => { if (err) throw err; })
+    })
 }
-
-function fromDir(startPath, filter, callback) {
-    console.log('\nStarting from dir ' + startPath + '/');
-
-    if (!fs.existsSync(startPath)) {
-        console.log("no dir ", startPath);
-        return;
-    }
-
-    var files = fs.readdirSync(startPath);
-    for (var i = 0; i < files.length; i++) {
-        var filename = path.join(startPath, files[i]);
-        var stat = fs.lstatSync(filename);
-        if (stat.isDirectory()) {
-            fromDir(filename, filter, callback); //recurse
-        }
-        else if (filter.test(filename)) {
-            console.log("\tFound: " + filename);
-            callback(filename);
-        }
-    };
-};
