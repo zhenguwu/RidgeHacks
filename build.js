36520acf814a2742eeaa76197c3ccbf5870add26
Shrey Pasricha <shrey.pasricha@gmail.com>
2019-04-13 14:43:37 -0400

    Now ncp copies only unminified files and is run in paralell, also added fs-extra, will replace ncp with fs-extra

--- build.js
+++ build.js
@@ -1,5 +1,4 @@
-var fs = require("fs");
-var path = require("path")
+var fse = require('fs-extra');
 var minify = require("html-minifier").minify;
 var autoprefixer = require("cssnano")
 var postcss = require("postcss")
@@ -27,37 +26,26 @@ const HTML_minifyOptions = {
     useShortDoctype: true
 }
 
-
-// Copy all files in ./src in order to transfer static files to dist as well as the minified ones.
-ncp("./src", "./dist", (err) => {
-
-    // On copying completion:
-    if (err) {
-        return console.error(err);
-    }
-
-    console.log("Finished copying.")
-    // Minify files in parallel by running their respective functions async 
-    async.parallel([
-        minifyFiles(/\.html$/, minifyHTML),
-        minifyFiles(/\.css$/, minifyCSS),
-        minifyFiles(/\.js/, minifyJS)
-    ]).catch((err) => { if (err.message !== "expected a function") console.error(err) });
-});
-
+// Minify files in parallel by running their respective functions async
+async.parallel([
+    // Copy all files that do not end in .js, .css or .html in ./src in order to transfer static files to ./dist as well as the minified ones.
+    ncp("./src", "./dist", { filter: /.+(?<!.js|.css|.html)$/ }, (err) => { if (err) { throw err; } else { console.log("Finished copying."); } }),
+    // Minify files
+    minifyFiles(/\.html$/, minifyHTML),
+    minifyFiles(/\.css$/, minifyCSS),
+    minifyFiles(/\.js/, minifyJS)
+]).catch((err) => { if (err.message !== "expected a function") console.error(err) });
 
 
 // Wrapper function that finds files and creates an async queue to pass them onto minification functions
 async function minifyFiles(filter, minificationFunction) {
     find.file(filter, "./src", (files) => {
-
         try {
             var queue = async.queue(minificationFunction, files.length);
         } catch (e) {
             if (e instanceof RangeError) console.error("ERROR!: Unable to find any files of file type: " + filter);
             return;
         }
-       
 
         // add files to the queue (batch-wise)
         queue.push(files, function (err) {
@@ -69,11 +57,11 @@ async function minifyFiles(filter, minificationFunction) {
 async function minifyHTML(src) {
     const outputPath = "dist" + src.substring(3);
 
-    fs.readFile(src, "utf8", function (err, data) {
+    fse.readFile(src, "utf8", function (err, data) {
         if (err) throw err;
         var htmlResult = minify(data, HTML_minifyOptions);
 
-        fs.writeFile("dist/index.html", htmlResult, "utf8", (err) => {
+        fse.outputFile("dist/index.html", htmlResult, "utf8", (err) => {
             if (err) throw err;
             console.log("minifyHTML:\tOutput minified HTML " + outputPath);
         });
@@ -83,15 +71,15 @@ async function minifyHTML(src) {
 async function minifyCSS(src) {
     const outputPath = "dist" + src.substring(3);
 
-    fs.readFile(src, (err, css) => {
+    fse.readFile(src, (err, css) => {
         if (err) throw err;
 
         postcss([autoprefixer])
             .process(css, { from: src, to: outputPath })
             .then(result => {
-                fs.writeFile(outputPath, result.css, (err) => { if (err) throw err; })
+                fse.outputFile(outputPath, result.css, (err) => { if (err) throw err; })
                 if (result.map) {
-                    fs.writeFile(outputPath, result.map, (err) => { if (err) throw err; })
+                    fse.outputFile(outputPath, result.map, (err) => { if (err) throw err; })
                 }
                 console.log("minifyCSS:\tOutput minified CSS " + outputPath);
             });
@@ -101,9 +89,9 @@ async function minifyCSS(src) {
 async function minifyJS(src) {
     const outputPath = "dist" + src.substring(3);
 
-    fs.readFile(src, "utf8", (err, js) => {
+    fse.readFile(src, "utf8", (err, js) => {
         if (err) throw err;
         console.log("minifyJS:\tOutput minified JS " + outputPath);
-        fs.writeFile(outputPath, UglifyJS.minify(js, UglifyOptions).code, (err) => { if (err) throw err; })
+        fse.outputFile(outputPath, UglifyJS.minify(js, UglifyOptions).code, (err) => { if (err) throw err; })
     });
 }
